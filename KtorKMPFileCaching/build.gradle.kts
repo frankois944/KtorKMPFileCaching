d1f1561effe7fb95e494ab33cd22a20304464717
frankois <dabonot.francois@gmail.com>
2024-12-23 11:32:05 +0100

    update deps
    Gradle -> 8.7.3
    Ktor -> 3.0.3
    kotlinCoroutines -> 1.10.1
    kotlinSerialization -> 1.7.3
    move publish plugin to toml
    remove warning

--- KtorKMPFileCaching/build.gradle.kts
+++ KtorKMPFileCaching/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinSerialization)
-    id("com.vanniktech.maven.publish") version "0.30.0"
+    alias(libs.plugins.publish)
 }
 
 
